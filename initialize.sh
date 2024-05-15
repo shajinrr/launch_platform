ce5d1ffcdffc1ea7b258dd92e8b3fc9b22b285e9
shajin <shajin@ubuntu.myguest.virtualbox.org>
2024-05-15 21:21:42 +0530

    ssh issue fix

--- initialize.sh
+++ initialize.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Create jenkins_data folder in the current directory
-mkdir -p jenkins_data
+# Define the directory paths
+jenkins_data_path="$(pwd)/jenkins_data"
+docker_path="$(pwd)/docker"
 
-# Change directory to jenkins_data
-cd jenkins_data || exit
+# Create jenkins_data folder
+mkdir -p "$jenkins_data_path"
 
 # Create .ssh directory
-mkdir -p .ssh
+mkdir -p "$jenkins_data_path/.ssh"
 
 # Generate SSH key pair and save in .ssh directory
-ssh-keygen -f .ssh/id_rsa -t rsa -N ''
+ssh-keygen -f "$jenkins_data_path/.ssh/id_rsa" -t rsa -N ''
 
 # Output success message
-echo "SSH key pair generated and saved in jenkins_data/.ssh directory."
+echo "SSH key pair generated and saved in $jenkins_data_path/.ssh directory."
 
+# Create ssh_pub_key folder under all folders under docker
+for folder in "$docker_path"/*/; do
+    ssh_pub_key_folder="$folder/ssh_pub_key"
+    mkdir -p "$ssh_pub_key_folder"
+    cp "$jenkins_data_path/.ssh/id_rsa.pub" "$ssh_pub_key_folder/id_rsa.pub"
+done
+
+# Output success message
+echo "ssh_pub_key folder created under all folders under $docker_path."
