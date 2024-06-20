1d0018c5eb32f08006d54a7c1a5ecc96de87cfed
shajin <shajin@ubuntu.myguest.virtualbox.org>
2024-06-20 20:21:05 +0530

    server connectivity fix

--- initialize.sh
+++ initialize.sh
@@ -11,7 +11,7 @@ mkdir -p "$jenkins_data_path"
 mkdir -p "$jenkins_data_path/.ssh"
 
 # Generate SSH key pair and save in .ssh directory
-ssh-keygen -f "$jenkins_data_path/.ssh/id_rsa" -t rsa -N ''
+ssh-keygen -f "$jenkins_data_path/.ssh/id_rsa" -t rsa-sha2-512 -N ''
 
 # Output success message
 echo "SSH key pair generated and saved in $jenkins_data_path/.ssh directory."
@@ -20,7 +20,7 @@ echo "SSH key pair generated and saved in $jenkins_data_path/.ssh directory."
 for folder in "$docker_path"/*/; do
     ssh_pub_key_folder="$folder/ssh_pub_key"
     mkdir -p "$ssh_pub_key_folder"
-    cp "$jenkins_data_path/.ssh/id_rsa.pub" "$ssh_pub_key_folder/id_rsa.pub"
+    sudo cp "$jenkins_data_path/.ssh/id_rsa.pub" "$ssh_pub_key_folder/id_rsa.pub"
 done
 
 # Output success message
